9a5c26941cac2fb79374db0a63bb16dd43ec1300
Samuel Adegbola <samueladegbola@Samuels-MacBook-Air.local>
2026-03-12 12:13:27 +0100

    file change

--- new/src/main.jsx
+++ new/src/main.jsx
@@ -1,7 +1,7 @@
-    import React from 'react';
-
-    export function Main(){
-        return(
-            <h1>Welcome to the Main Content!</h1>
-        )
-    }
+ import React from 'react';
+import ReactDOM from 'react-dom/client';
+ ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

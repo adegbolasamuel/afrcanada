29db7e46a21cad530d7784d3af8fbae28f70fbaf
Kingsley <olorunfemiolakunle44@gmail.com>
2026-03-15 06:13:24 +0100

    fix: build errors and Vercel deployment setup

--- new/src/App.jsx
+++ new/src/App.jsx
@@ -1,27 +1,29 @@
-import { Header } from './Header'; 
-import HeroSection from './HeroSection'; 
-import AboutSection from './AboutSection';
-import ExpoDetailsSection from './ExpoDetailsSection';
-import IndigenousPartnershipSection from './IndigenousPartnershipSection';
-import './header.css'; 
-import "./HeroSection.css"
-import "./AboutSection.css"
-import "./IndigenousPartnershipSection.css"
-import Footer from './Footer';
-import "./App.css"
-import "./Footer.css"
-import PartnerNewsCTASection from './PartnerNewsCTASection';
+import { Header } from "./header";
+import HeroSection from "./HeroSection";
+import AboutSection from "./AboutSection";
+import ExpoDetailsSection from "./ExpoDetailsSection";
+import IndigenousPartnershipSection from "./IndigenousPartnershipSection";
+import "./Header.css";
+import "./HeroSection.css";
+import "./AboutSection.css";
+import "./IndigenousPartnershipSection.css";
+import "./ExpoDetailsSection.css";
+import "./PartnerNewsCTASection.css";
+import Footer from "./Footer";
+import "./App.css";
+import "./Footer.css";
+import PartnerNewsCTASection from "./PartnerNewsCTASection";
 
-export function App() { 
+export function App() {
   return (
     <div className="App">
       <Header />
-      <HeroSection /> 
-       <AboutSection />
-       <ExpoDetailsSection />
-       <IndigenousPartnershipSection />
-       <PartnerNewsCTASection />
-       <Footer />
+      <HeroSection />
+      <AboutSection />
+      <ExpoDetailsSection />
+      <IndigenousPartnershipSection />
+      <PartnerNewsCTASection />
+      <Footer />
     </div>
   );
 }
